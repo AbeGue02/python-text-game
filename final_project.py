87c0fd7c32b71d74ce425de03026833f4794e197
AbeGue02 <abrahamgue02@gmail.com>
2025-02-21 00:45:47 -0500

    Changes rooms structure to fit more content

--- final_project.py
+++ final_project.py
@@ -1,15 +1,49 @@
 # Abraham Guerrero
 
+inventory = []
+
 #This dictionary links a room to other rooms.
 rooms = {
-    'Entrance': {'north': 'Courtyard'},
-    'Courtyard': {'north': 'City Gate', 'south': 'Entrance'},
-    'City Gate': {'east': 'Hallway', 'west': 'Statue', 'north': 'City', 'south': 'Courtyard'},
-    'Statue': {'west': 'Cathedral', 'east': 'City Gate'},
-    'Cathedral': {'east': 'Statue'},
-    'Hallway': {'west': 'City Gate', 'east': 'Library'},
-    'Library': {'west': 'Hallway'},
-    'City': {'south': 'City Gate'},
+    'Entrance': {
+        'move': {'north': 'Courtyard'},
+        'items': [],
+        'description': ''
+    },
+    'Courtyard': {
+        'move': {'north': 'City Gate', 'south': 'Entrance'},
+        'items': [],
+        'description': ''
+    },
+    'City Gate': {
+        'move': {'east': 'Hallway', 'west': 'Statue', 'north': 'City', 'south': 'Courtyard'},
+        'items': [],
+        'description': ''
+    },
+    'Statue': {
+        'move': {'west': 'Cathedral', 'east': 'City Gate'},
+        'items': [],
+        'description': ''
+    },
+    'Cathedral': {
+        'move': {'east': 'Statue'},
+        'items': [],
+        'description': ''
+    },
+    'Hallway': {
+        'move': {'west': 'City Gate', 'east': 'Library'},
+        'items': [],
+        'description': ''
+    },
+    'Library': {
+        'move': {'west': 'Hallway'},
+        'items': [],
+        'description': ''
+    },
+    'City': {
+        'move': {'south': 'City Gate'},
+        'items': [],
+        'description': ''
+    },
 }
 
 #Starting room
@@ -18,8 +52,8 @@ current_room = rooms['Entrance']
 # Function prints available movement options in a room
 def print_available_movement():
     print('Type the direction in which you would like to go:')
-    for move_option in current_room.keys():
-        print(f'[{move_option.capitalize()}]: {current_room[move_option]}')
+    for move_option, destination in current_room['move'].items():
+        print(f'[{move_option.capitalize()}]: {destination}')
     print('To end game, type "Exit"')
 
 # This function handles the movement from one room to another
